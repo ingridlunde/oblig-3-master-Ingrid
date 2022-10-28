d5f9adc20022769069a80adde07c12cc49ae045e
ingridlunde <ingrid.lunde91@gmail.com>
2022-10-28 09:01:11 +0200

    Endret i oppgavefilen, noe jeg ikke skulle gjøre. Har endret tilbake nå

--- src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/Oppgave.java
@@ -5,14 +5,4 @@ public interface Oppgave<T>  // legges under hjelpeklasser
 {
     void utførOppgave(T t);    // f.eks. utskrift til konsollet
 
-    public static <T> Oppgave<T> konsollutskrift()  // en konstruksjonsmetode
-    {
-        return t -> System.out.print(t + " ");        // et lambda-uttrykk
-    }
-
-    public static <T> Oppgave<T> konsollutskrift(String format)
-    {
-        return t -> System.out.printf(format, t);
-    }
-
 }
